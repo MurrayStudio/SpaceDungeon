105c161a5c8564718bf67daec733560d8ab90780
Murray Studio <sushishamus@gmail.com>
2016-03-22 22:47:11 -0700

    added hover ui to buttons

---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GamePlayController : MonoBehaviour {
+
+    public Image popUpImage;
+    public Text popUpText;
+    public GameObject popUP;
+
+    bool popUPEnabled = false;
+
+	// Use this for initialization
+	void Start () {
+        popUP.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void showPopUp ()
+    {
+        if (!popUPEnabled)
+        {
+            popUP.SetActive(true);
+            popUP.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+            popUPEnabled = true;
+        }
+    }
+
+    public void hidePopUp()
+    {
+        popUP.SetActive(false);
+        popUPEnabled = false;
+    }
+}
