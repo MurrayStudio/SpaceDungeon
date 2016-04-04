af19c44408ac961eaf8ac5d35426fec546d1bb66
Murray Studio <sushishamus@gmail.com>
2016-04-04 11:01:16 -0700

    fixed menu and ui scaling

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Button startBtn, exitBtn;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    public void Play()
+    {
+        //SceneManager.LoadScene("Main");
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
